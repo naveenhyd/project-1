449c985082bce16e4f8447b43a6d4652e6c385ed
Naveen <p6naveen@gmail.com>
2016-02-13 13:00:12 +0530

    calculator code

---
+++ app/libraries/Calculator.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Libraries;
+
+class Calculator {
+
+    function __construct() {
+        
+    }
+
+    public function add($x, $y) {
+        if (!is_numeric($x) || !is_numeric($y)) {
+            throw new \InvalidArgumentException;
+        }
+        return $x + $y;
+    }
+
+}
